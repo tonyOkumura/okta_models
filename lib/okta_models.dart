f60aa89691dd4b50722deb2d69de461ce3f1074c
Tokumura <tokumura@ya.ru>
2025-09-10 13:48:03 +0300

    Refactor okta_models: reorganize exports and remove deprecated calendar, contact, event, note, and task models

--- lib/okta_models.dart
+++ lib/okta_models.dart
@@ -1,3 +1,60 @@
 library okta_models;
 
-export 'src/user.dart';
+//user models
+export 'src/user/user.dart';
+
+//contact models
+export 'src/contact/contact.dart';
+export 'src/contact/contact_group.dart';
+export 'src/contact/contact_group_member.dart';
+
+//conversation models
+export 'src/conversation/conversation.dart';
+export 'src/conversation/conversation_folder.dart';
+export 'src/conversation/conversation_participant.dart';
+
+//message models
+export 'src/message/message.dart';
+export 'src/message/message_attachment.dart';
+export 'src/message/message_mention.dart';
+export 'src/message/message_reaction.dart';
+export 'src/message/message_read.dart';
+
+//task models
+export 'src/task/task.dart';
+export 'src/task/task_assignee.dart';
+export 'src/task/task_attachment.dart';
+export 'src/task/task_checklist_item.dart';
+export 'src/task/task_comment.dart';
+export 'src/task/task_dependency.dart';
+export 'src/task/task_label_map.dart';
+export 'src/task/task_label.dart';
+export 'src/task/task_log.dart';
+export 'src/task/task_watcher.dart';
+
+//event models
+export 'src/event/event.dart';
+export 'src/event/event_attachment.dart';
+export 'src/event/event_attendee.dart';
+export 'src/event/event_comment.dart';
+export 'src/event/event_log.dart';
+export 'src/event/event_reminder.dart';
+export 'src/event/event_tag.dart';
+
+//file models
+export 'src/file/file.dart';
+
+//note models
+export 'src/note/note.dart';
+export 'src/note/note_attachment.dart';
+export 'src/note/note_checklist_item.dart';
+export 'src/note/note_collaborator.dart';
+export 'src/note/note_comment.dart';
+export 'src/note/note_folder.dart';
+export 'src/note/note_log.dart';
+export 'src/note/note_notebook.dart';
+export 'src/note/note_tag_map.dart';
+export 'src/note/note_tag.dart';
+
+//notification models
+export 'src/notification/notification.dart';
